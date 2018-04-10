93c6ffda392dcc252034eef8b4ec0d8c73d9b1ac
Jérémie Loosfelt <jloosfelt@gmail.com>
2018-04-10 10:01:23 +0200

    Update event.rb

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-  has_many :subscribers, :foreign_key => "event_id", :class_name => "Event"
+  has_many :subscribers # pas besoin =>, :foreign_key => "event_id", :class_name => "Event"
 
   validates :title, presence: {
     message: "le nom doit être renseigné."
